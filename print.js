8e7c49b93e36d8fa7e030891ea1679c74b252e33
Click Quick <editeur.batna@gmail.com>
2026-03-13 20:24:02 +0100

    Update print.js

--- print.js
+++ print.js
@@ -241,8 +241,8 @@
     /* CSS */
     var css =
       '*,*::before,*::after{box-sizing:border-box;margin:0;padding:0;}' +
-      '@page{size:' + paperMm + 'mm auto;margin:4mm;}' +
-      'html,body{width:100%;max-width:' + (paperMm-8) + 'mm;background:#fff;' +
+      '@page{size:' + paperMm + 'mm auto;margin:2mm;}' +
+      'html,body{width:100%;max-width:' + (paperMm-4) + 'mm;background:#fff;' +
       'font-family:"Courier New",Courier,monospace;' +
       'font-size:' + fontSize + ';font-weight:800;direction:rtl;color:#000;}' +
       '.content{width:' + printW + 'mm;margin:0 auto;}' +
